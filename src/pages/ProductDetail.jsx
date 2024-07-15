119091fe28784ce4834692a74cc6610bbbd8cd23
Sergio <norgestream@gmail.com>
2024-07-15 13:39:37 +0300

    add loader to productDetail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,12 @@
 import { useParams, useOutletContext, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styles from './ProductDetail.module.css';
+import Loader from '../components/Loader';
 
 const ProductDetail = () => {
   const [product, setProduct] = useState({});
   const [items, setItems] = useOutletContext([]);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -15,9 +17,16 @@ const ProductDetail = () => {
   let { productId } = useParams();
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${productId}`)
-      .then((res) => res.json())
-      .then((json) => setProduct(json));
+    const fetchData = async () => {
+      setLoading(true);
+      const data = await fetch(`https://fakestoreapi.com/products/${productId}`);
+      const result = await data.json();
+      setLoading(false);
+
+      setProduct(result);
+    }
+
+    fetchData();
   }, [productId]);
 
   const onAddItem = () => {
@@ -37,17 +46,19 @@ const ProductDetail = () => {
   };
 
   return (
-    <div className={styles.wraper}>
-      <div className={styles.titleWraper}>
-        <h3 className={styles.title}>{product.title}</h3>
-        <p className={styles.category}>Category - {product.category}</p>
-        <button onClick={onNavigate} className={styles.button}>X</button>
-      </div>
-      <img src={product.image} alt={product.title} />
-      <p>{product.description}</p>
-      <p className={styles.price}>Price {product.price}$</p>
-      <button onClick={onAddItem} className={`${styles.button} ${styles.addButton}`}>Add to Cart</button>
-    </div>
+    <>
+      {loading ? <Loader /> : <div className={styles.wraper}>
+        <div className={styles.titleWraper}>
+          <h3 className={styles.title}>{product.title}</h3>
+          <p className={styles.category}>Category - {product.category}</p>
+          <button onClick={onNavigate} className={styles.button}>X</button>
+        </div>
+        <img src={product.image} alt={product.title} />
+        <p>{product.description}</p>
+        <p className={styles.price}>Price {product.price}$</p>
+        <button onClick={onAddItem} className={`${styles.button} ${styles.addButton}`}>Add to Cart</button>
+      </div>}
+    </>
   );
 };
 
