cf0fe56d7abc9f7ea670d77b1f4e7bfc7bb58f8e
Sergio <norgestream@gmail.com>
2024-07-23 00:08:55 +0300

    centered text if final page

--- src/pages/Final.jsx
+++ src/pages/Final.jsx
@@ -3,7 +3,7 @@ import styles from './Final.module.css';
 const Final = () => {
   return (
     <div className={styles.container}>
-      <h1>Thank you for your order!</h1>
+      <h2>Thank you for your order!</h2>
       <p>We are looking forward to delivering it in a few days</p>
       <p>Hope to see you soon again :)</p>
     </div>
