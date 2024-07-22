a9eae6c35243be16b7fa038a5c0a4b556a1adf47
Sergio <norgestream@gmail.com>
2024-07-22 16:14:10 +0300

    add some styling to the button when adding item to cart

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,30 @@
 import styles from '../components/Item.module.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 
 const Item = ({title, price, image, onAddItem, item}) => {
   const navigate = useNavigate();
+  const [addedItem, setAddedItem] = useState(false);
+  let buttonName = 'Add to cart';
+  let bgc = '#ed1c24'
+
+  const addItemHandler = () => {
+    onAddItem(item);
+    setAddedItem(true);
+  }
+
+  if (addedItem) {
+    buttonName = 'In the cart';
+    bgc='violet';
+  }
 
   return (
     <div className={styles.good}>
       <img src={image} alt={item.title} onClick={() => navigate(`/products/${item.id}`)}/>
       <p className={styles.title}>{title}</p>
       <p className={styles.price}>{price} $</p>
-      <button type='text' className={styles.add} onClick={() => onAddItem(item)}>Add to Cart</button>
+      <button type='text' className={styles.add} onClick={addItemHandler} style={{backgroundColor: bgc}}>{buttonName}</button>
     </div>
   )
 }
